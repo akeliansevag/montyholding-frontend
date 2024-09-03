6825ce390027143da028db189bb668b80231e33a
montyholdingdev <155431830+montyholdingdev@users.noreply.github.com>
2024-09-03 22:00:44 +0300

    .

--- pages/index.vue
+++ pages/index.vue
@@ -9,7 +9,7 @@
         description="A global leader in telecommunications, Monty Mobile delivers innovative technology and communication solutions to mobile network operators, enterprises, and service providers worldwide."
         :directors="BODMontyMobile" :button="{ link: 'https://montymobile.com', text: 'Visit Monty Mobile Website' }" />
         <BoardOfDirectors :dark="true" title="Monty Finance board of directors"
-        description="Monty Finance SAL is a local financial technology service provider. They are dedicated to meeting the needs of underserved,and unbanked communities through a comprehensive range of services across various verticals and markets."
+        description="Monty Finance is a local financial technology service provider. They are dedicated to meeting the needs of underserved,and unbanked communities through a comprehensive range of services across various verticals and markets."
         :directors="BODMontyFinance"
         :button="{ link: 'https://montyfinance.co.uk', text: 'Visit Monty Finance Website' }" />
     </div>
