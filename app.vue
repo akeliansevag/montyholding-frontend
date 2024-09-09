c12613a72c2098c5ff9e642b0ab42034331da981
montyholdingdev <155431830+montyholdingdev@users.noreply.github.com>
2024-09-09 15:59:32 +0300

    .

--- app.vue
+++ app.vue
@@ -9,5 +9,10 @@
 <script setup>
 useSeoMeta({
   title: 'Monty Holding',
-});
+  ogTitle: 'Monty Holding',
+  description: 'Join us in our journey as we create value and change the world',
+  ogDescription: 'Join us in our journey as we create value and change the world',
+  ogImage: 'https://montyholding.com/ogimage.webp',
+  twitterCard: 'https://montyholding.com/ogimage.webp',
+})
 </script>
